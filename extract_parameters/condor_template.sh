6e3cfe0eb2f0c81a16a06ef5e4607ebdd1212257
Gustavo <diemort@gmail.com>
2023-03-16 22:20:40 +0100

    adding support for HTCondor

---
+++ extract_parameters/condor_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cmssw=$1
+scram project $cmssw
+cd $cmssw/src/
+eval `scramv1 runtime -sh`
+scram b -j8
+cd -
+cmsRun $2
+rm -rf CMSSW*
+
