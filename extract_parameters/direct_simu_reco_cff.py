ba7dfc9c23482428928cd06b67ef5366515b70d5
Gustavo <diemort@gmail.com>
2023-03-17 15:53:26 +0100

    correcting year in profile in DS template

--- extract_parameters/direct_simu_reco_cff.py
+++ extract_parameters/direct_simu_reco_cff.py
@@ -3,6 +3,7 @@ era = Run3
 
 def LoadConfig(process):
   global config
-  import Validation.CTPPS.simu_config.year_2021_cff as config
-  process.load("Validation.CTPPS.simu_config.year_2021_cff")
-  process.ctppsCompositeESSource.periods = [config.profile_2021_default]
+  import Validation.CTPPS.simu_config.year_$year_cff as config
+  process.load("Validation.CTPPS.simu_config.year_$year_cff")
+  process.ctppsCompositeESSource.periods = [config.profile_$year_default]
+  process.profile_$year_default.ctppsLHCInfo.beamEnergy = $Ebeam
